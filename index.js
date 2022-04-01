d813bf2225bec0f7968f0cdb8bcd212247c8b861
yoyoyojoe <86270711+yoyoyojoe@users.noreply.github.com>
2022-03-31 23:19:32 -0700

    use uniform header, styles.css & index.js

--- index.js
+++ index.js
@@ -33,6 +33,36 @@ function letsGoCatchEmAll(url) {
   return false;
 }
 
+function validateForm() {
+  const name = document.getElementById('name').value;
+  if (name === '') {
+    document.querySelector('.status').innerHTML = 'Name cannot be empty';
+    return false;
+  }
+  const email = document.getElementById('email').value;
+  if (email === '') {
+    document.querySelector('.status').innerHTML = 'Email cannot be empty';
+    return false;
+  }
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (!re.test(email)) {
+    document.querySelector('.status').innerHTML = 'Email format invalid';
+    return false;
+  }
+
+  const subject = document.getElementById('subject').value;
+  if (subject === '') {
+    document.querySelector('.status').innerHTML = 'Subject cannot be empty';
+    return false;
+  }
+  const message = document.getElementById('message').value;
+  if (message === '') {
+    document.querySelector('.status').innerHTML = 'Message cannot be empty';
+    return false;
+  }
+  return false;
+}
+
 document.addEventListener('pointerdown', (e) => {
   // when clicking on the specific button, (using selector & data attribute)
   const dropdownBtn = e.target.matches('[data-dropdown-button]');
@@ -54,7 +84,7 @@ document.addEventListener('pointerdown', (e) => {
   });
 
   // * contact us button
-  const contactBtn = e.target.matches('.contact');
+  const contactBtn = e.target.matches('#contact');
 
   if (contactBtn) {
     // fetch(`http://api.ipstack.com/check?access_key=${ACCESS_KEY}`)
