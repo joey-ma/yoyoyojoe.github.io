8d6cfb8d939e3e53296de41f4748fbbdadac3ead
yoyoyojoe <86270711+yoyoyojoe@users.noreply.github.com>
2022-03-31 18:40:46 -0700

    updated index.html
    cleaned up

---
+++ blog/index.js
@@ -0,0 +1,112 @@
+let catchWindow = null; let
+  previousURL;
+
+// * loading catch game when catch-button onclick
+function letsGoCatchEmAll(url) {
+  const innerWidth = 950;
+  const innerHeight = 930;
+  const windowFeatures = `popup,
+  screenX = ${((window.screen.availWidth - innerWidth) / 2)},
+  screenY = ${(window.screen.availHeight - innerHeight) / 2}, 
+  innerWidth=${innerWidth}, innerHeight=${innerHeight}`;
+  // screenX = ${((window.screen.width - window.outerWidth) / 2)},
+  // screenY = ${((window.screen.height * 0.1) / 2)},
+
+  if (catchWindow === null || catchWindow.closed) {
+    catchWindow = window.open(
+      url,
+      'CatchGameWindow',
+      windowFeatures,
+    );
+    console.log(catchWindow);
+  } else if (previousURL !== url) {
+    catchWindow = window.open(
+      url,
+      'CatchGameWindow',
+      windowFeatures,
+    );
+    catchWindow.focus();
+  } else {
+    catchWindow.focus();
+  }
+  previousURL = url;
+  return false;
+}
+
+// * not working due to "405 method not allowed", mail.php is not currently used
+/* function sendEmail(name, email, subject, message) {
+  document.querySelector('.status').innerHTML = 'Working...';
+
+  const xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    console.log(this.readyState);
+    if (this.readyState === 1) {
+      document.querySelector('.status').innerHTML = 'Not quite sent yet...';
+      console.log('Not quite sent yet...');
+    }
+    if (this.readyState === 2) {
+      document.querySelector('.status').innerHTML = 'Sending...';
+      console.log('Sending...');
+    }
+    if (this.readyState === 4) {
+      console.dir(document.querySelectorAll('input'));
+      document.querySelectorAll('input').forEach((el) => el.value = '');
+      document.getElementById('message').value = '';
+      document.querySelector('.status').innerHTML = 'Message sent!';
+      console.log('Message Sent');
+    }
+  };
+  xhttp.open('POST', 'mail.php', true);
+  xhttp.send();
+} */
+
+function validateForm() {
+  const name = document.getElementById('name').value;
+  if (name === '') {
+    document.querySelector('.status').innerHTML = 'Name cannot be empty';
+    return false;
+  }
+  const email = document.getElementById('email').value;
+  if (email === '') {
+    document.querySelector('.status').innerHTML = 'Email cannot be empty';
+    return false;
+  }
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (!re.test(email)) {
+    document.querySelector('.status').innerHTML = 'Email format invalid';
+    return false;
+  }
+
+  const subject = document.getElementById('subject').value;
+  if (subject === '') {
+    document.querySelector('.status').innerHTML = 'Subject cannot be empty';
+    return false;
+  }
+  const message = document.getElementById('message').value;
+  if (message === '') {
+    document.querySelector('.status').innerHTML = 'Message cannot be empty';
+    return false;
+  }
+  return false;
+}
+
+document.addEventListener('pointerdown', (e) => {
+  // when clicking on the specific button, (using selector & data attribute)
+  const dropdownBtn = e.target.matches('[data-dropdown-button]');
+
+  let currentDropdownContainer; // to store the current dropdown clicked
+  // if dropdownBtn clicked && if the parent / closet element is 'data-dropdown' -> leave it open
+  if (dropdownBtn) {
+    currentDropdownContainer = e.target.closest('[data-dropdown-container]');
+    // hide or show this dropdown depending on if it's opened or not
+    currentDropdownContainer.classList.toggle('active');
+  }
+  // if not clicked && if the parent / closet element is not null -> close the dropdown menu
+  if (!dropdownBtn && e.target.closest('[data-dropdown-container]') != null) return;
+
+  // closes all the dropdowns that are already opened, except for the one just opened
+  document.querySelectorAll('[data-dropdown-container].active').forEach((dropdown) => {
+    if (dropdown === currentDropdownContainer) return;
+    dropdown.classList.remove('active');
+  });
+});
