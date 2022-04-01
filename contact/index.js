1da0b4fa16b833e2586831db7ec31d434af786d9
yoyoyojoe <86270711+yoyoyojoe@users.noreply.github.com>
2022-03-31 18:43:52 -0700

    updated index.js
    - cleaned up code
    - tested popup window position for catchGameWindow
    - updated contact button functionality
    - commented ipstack functionality

--- contact/index.js
+++ contact/index.js
@@ -1,4 +1,40 @@
-function sendEmail(name, email, subject, message) {
+let catchWindow = null; let
+  previousURL;
+
+// * loading catch game when catch-button onclick
+function letsGoCatchEmAll(url) {
+  const innerWidth = 950;
+  const innerHeight = 930;
+  const windowFeatures = `popup,
+  screenX = ${((window.screen.availWidth - innerWidth) / 2)},
+  screenY = ${(window.screen.availHeight - innerHeight) / 2}, 
+  innerWidth=${innerWidth}, innerHeight=${innerHeight}`;
+  // screenX = ${((window.screen.width - window.outerWidth) / 2)},
+  // screenY = ${((window.screen.height * 0.1) / 2)},
+
+  if (catchWindow === null || catchWindow.closed) {
+    catchWindow = window.open(
+      url,
+      'CatchGameWindow',
+      windowFeatures,
+    );
+    console.log(catchWindow);
+  } else if (previousURL !== url) {
+    catchWindow = window.open(
+      url,
+      'CatchGameWindow',
+      windowFeatures,
+    );
+    catchWindow.focus();
+  } else {
+    catchWindow.focus();
+  }
+  previousURL = url;
+  return false;
+}
+
+// * not working due to "405 method not allowed", mail.php is not currently used
+/* function sendEmail(name, email, subject, message) {
   document.querySelector('.status').innerHTML = 'Working...';
 
   const xhttp = new XMLHttpRequest();
@@ -22,7 +58,7 @@ function sendEmail(name, email, subject, message) {
   };
   xhttp.open('POST', 'mail.php', true);
   xhttp.send();
-}
+} */
 
 function validateForm() {
   const name = document.getElementById('name').value;
@@ -51,6 +87,26 @@ function validateForm() {
     document.querySelector('.status').innerHTML = 'Message cannot be empty';
     return false;
   }
-
-  sendEmail(name, email, subject, message);
+  return false;
 }
+
+document.addEventListener('pointerdown', (e) => {
+  // when clicking on the specific button, (using selector & data attribute)
+  const dropdownBtn = e.target.matches('[data-dropdown-button]');
+
+  let currentDropdownContainer; // to store the current dropdown clicked
+  // if dropdownBtn clicked && if the parent / closet element is 'data-dropdown' -> leave it open
+  if (dropdownBtn) {
+    currentDropdownContainer = e.target.closest('[data-dropdown-container]');
+    // hide or show this dropdown depending on if it's opened or not
+    currentDropdownContainer.classList.toggle('active');
+  }
+  // if not clicked && if the parent / closet element is not null -> close the dropdown menu
+  if (!dropdownBtn && e.target.closest('[data-dropdown-container]') != null) return;
+
+  // closes all the dropdowns that are already opened, except for the one just opened
+  document.querySelectorAll('[data-dropdown-container].active').forEach((dropdown) => {
+    if (dropdown === currentDropdownContainer) return;
+    dropdown.classList.remove('active');
+  });
+});
