ec1c0261f12505b92d49b4cac207b7933d458c5f
yoyoyojoe <jpma.family@gmail.com>
2022-04-27 09:20:30 -0700

    Update urls.py

--- django/polls/urls.py
+++ django/polls/urls.py
@@ -3,10 +3,10 @@ from django.urls import path
 from . import views
 
 #* Version 2
-app_name = 'polls'
+app_name = ''
 urlpatterns = [
   path('', views.IndexView.as_view(), name='index'),
-  path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+  path('polls/<int:pk>/', views.DetailView.as_view(), name='detail'),
   path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
   path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
